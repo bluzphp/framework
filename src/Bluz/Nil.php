6d50bf802de14d502c14fbfa900c2f0db5ea521d
Anton Shevchuk <AntonShevchuk@gmail.com>
2013-01-15 09:56:17 +0200

    Added Nil (null) class

---
+++ src/Bluz/Nil.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Copyright (c) 2012 by Bluz PHP Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * @namespace
+ */
+namespace Bluz;
+
+/**
+ * It's just null class
+ *
+ * @category Bluz
+ * @package  Nil
+ *
+ * @author   Anton Shevchuk
+ * @created  15.01.13 09:50
+ */
+class Nil
+{
+    /**
+     * __call
+     *
+     * @return null
+     */
+    public function __call()
+    {
+        return null;
+    }
+
+    /**
+     * __set
+     *
+     * @return null
+     */
+    public function __set()
+    {
+        return null;
+    }
+}

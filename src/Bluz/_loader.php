fe6183ea453bc807b3a6097370ae0104f2d76a3d
Anton Shevchuk <Anton.Shevchuk@gmail.com>
2013-03-04 18:52:40 +0200

    Added ViewInterface to _loader.php

--- src/Bluz/_loader.php
+++ src/Bluz/_loader.php
@@ -56,5 +56,6 @@ require_once 'Request/AbstractRequest.php';
 require_once 'Router/Router.php';
 require_once 'Session/Session.php';
 require_once 'Session/Store/AbstractStore.php';
+require_once 'View/ViewInterface.php';
 require_once 'View/View.php';
 require_once 'View/Layout.php';

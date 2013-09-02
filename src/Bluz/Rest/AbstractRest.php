19affa8d3796db83f4c5fc355fa2280c8e14baa6
Anton Shevchuk <Anton.Shevchuk@gmail.com>
2013-09-02 14:07:12 +0300

    Fixed codestyle

--- src/Bluz/Rest/AbstractRest.php
+++ src/Bluz/Rest/AbstractRest.php
@@ -69,11 +69,6 @@ abstract class AbstractRest
         // rewrite REST with "method" param
         $this->method = strtoupper($request->getParam('_method', $request->getMethod()));
 
-        // try to get uid
-        $uri = parse_url($request->getRequestUri(), PHP_URL_PATH);
-        $url = app()->getRouter()->url(null, null);
-        $this->id = trim(substr($uri, strlen($url)), '/');
-
         // get all params
         $allParams = $request->getAllParams();
 
@@ -161,6 +156,11 @@ abstract class AbstractRest
     public function __invoke()
     {
         $request = app()->getRequest();
+
+        // try to get uid
+        $uri = parse_url($request->getRequestUri(), PHP_URL_PATH);
+        $url = app()->getRouter()->url(null, null);
+        $this->id = trim(substr($uri, strlen($url)), '/');
 
         // GET    /module/rest/   -> collection
         // GET    /module/rest/id -> get item

eb15d1dfdcee65429a4294fdda427b2c22e84370
Mak-Di <makarov.dmitriy@gmail.com>
2014-01-10 10:08:47 +0200

    Update Cache.php

--- src/Cache/Cache.php
+++ src/Cache/Cache.php
@@ -58,7 +58,7 @@ class Cache implements CacheInterface, TagableInterface
     protected function checkOptions()
     {
         // check cache Adapter instance and settings for initialize it
-        if (!isset($this->options['cacheAdapter']) && !isset($this->options['settings']['cacheAdapter'])) {
+        if (!isset($this->options['cacheAdapter'], $this->options['settings']['cacheAdapter'])) {
             throw new ConfigException(
                 "Missed `cacheAdapter` option in `cache` configuration. <br/>\n" .
                 "Read more: <a href='https://github.com/bluzphp/framework/wiki/Cache'>".

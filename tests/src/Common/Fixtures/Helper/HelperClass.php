05625fd0d2d4a938dc00cd78d57bae4d6b8fab7b
AntonShevchuk <AntonShevchuk@gmail.com>
2014-01-14 12:37:42 +0200

    Added tests for Helper trait

---
+++ tests/src/Common/Fixtures/Helper/HelperClass.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @copyright Bluz PHP Team
+ * @link https://github.com/bluzphp/framework
+ */
+
+/**
+ * Example of Class helper
+ * 
+ * @author   Anton Shevchuk
+ * @created  14.01.14 12:27
+ */
+class ExampleClass
+{
+    /**
+     * Callable
+     *
+     * @param mixed $argument
+     * @return mixed
+     */
+    public function __invoke($argument)
+    {
+        return $argument;
+    }
+}
+
+return new ExampleClass();
---
+++ tests/src/Common/Fixtures/Helper/HelperInvalidFunction.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @copyright Bluz PHP Team
+ * @link https://github.com/bluzphp/framework
+ */
+
+/**
+ * Example of invalid helper
+ * 
+ * @author   Anton Shevchuk
+ * @created  14.01.14 11:39
+ */
+return rand(1, 42);

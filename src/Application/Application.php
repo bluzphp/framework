139adb1e837a3cf9efc704ebb6c915c656256b10
AntonShevchuk <AntonShevchuk@gmail.com>
2014-05-29 13:54:36 +0300

    Set Exception to Response before dispatch error controller

--- src/Application/Application.php
+++ src/Application/Application.php
@@ -722,6 +722,7 @@ abstract class Application
                 $response->setHeader('Refresh', '15; url=' . $request->getRequestUri());
             }
         } catch (\Exception $e) {
+            $response->setException($e);
 
             $dispatchResult = $this->dispatch(
                 Router::ERROR_MODULE,
@@ -737,9 +738,7 @@ abstract class Application
                 $dispatchResult = $this->getLayout();
             }
 
-            $this->getResponse()
-                ->setException($e)
-                ->setCode($e->getCode())
+            $response->setCode($e->getCode())
                 ->setBody($dispatchResult);
         }
 

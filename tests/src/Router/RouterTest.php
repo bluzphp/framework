232f4c62bfe970f73516c2d49ffe9f68ea8805ed
AntonShevchuk <AntonShevchuk@gmail.com>
2014-08-12 18:50:51 +0300

    Updated Router tests

--- tests/src/Router/RouterTest.php
+++ tests/src/Router/RouterTest.php
@@ -26,40 +26,29 @@ class RouterTest extends TestCase
     /**
      * testRouterUrl
      *
-     * @dataProvider providerForEquals
+     * @dataProvider providerForDefaultRoutes
      * @param string $url
      * @param string $module
      * @param string $controller
      * @param array $params
      */
-    public function testRouterUrl($url, $module, $controller, $params)
+    public function testRouterUrl($url, $module, $controller, $params = array())
     {
         $this->assertEquals($url, $this->getApp()->getRouter()->url($module, $controller, $params));
     }
 
     /**
      * Test Router Url for custom controller route
+     * 
+     * @dataProvider providerForCustomRoutes
+     * @param string $url
+     * @param string $module
+     * @param string $controller
+     * @param array $params
      */
-    public function testRouterUrlWithCustomControllerRoute()
+    public function testRouterUrlWithCustomControllerRoute($url, $module, $controller, $params = array())
     {
-        $router = $this->getApp()->getRouter();
-
-        $this->assertEquals(
-            '/another-route.html',
-            $router->url('test', 'route-static')
-        );
-        $this->assertEquals(
-            '/test/param/42/',
-            $router->url('test', 'route-with-param', ['a' => 42])
-        );
-        $this->assertEquals(
-            '/foo-bar-baz/',
-            $router->url('test', 'route-with-params', ['a' => 'foo', 'b' => 'bar', 'c' => 'baz'])
-        );
-        $this->assertEquals(
-            '/test/route-with-other-params/about',
-            $router->url('test', 'route-with-other-params', ['alias' => 'about'])
-        );
+        $this->assertEquals($url, $this->getApp()->getRouter()->url($module, $controller, $params));
     }
 
     /**
@@ -89,15 +78,32 @@ class RouterTest extends TestCase
     /**
      * @return array
      */
-    public function providerForEquals()
+    public function providerForDefaultRoutes()
     {
         return array(
-            array('/test/test', 'test', 'test', array()),
-            array('/test/test/foo/bar', 'test', 'test', ['foo'=>'bar']),
-            array('/test', 'test', Router::DEFAULT_CONTROLLER, array()),
-            array('/test/'.Router::DEFAULT_CONTROLLER.'/foo/bar', 'test', Router::DEFAULT_CONTROLLER, ['foo'=>'bar']),
-            array('/'.Router::DEFAULT_MODULE.'/test', Router::DEFAULT_MODULE, 'test', array()),
-            array('/'.Router::DEFAULT_MODULE.'/test/foo/bar', Router::DEFAULT_MODULE, 'test', ['foo'=>'bar']),
+            ['/test/test', 'test', 'test', array()],
+            ['/test/test/foo/bar', 'test', 'test', ['foo'=>'bar']],
+            ['/test/test?foo%5B0%5D=bar&foo%5B1%5D=baz', 'test', 'test', ['foo'=> ['bar', 'baz']]],
+            ['/test', 'test', null, array()],
+            ['/test', 'test', Router::DEFAULT_CONTROLLER, array()],
+            ['/test/'.Router::DEFAULT_CONTROLLER.'/foo/bar', 'test', Router::DEFAULT_CONTROLLER, ['foo'=>'bar']],
+            ['/'.Router::DEFAULT_MODULE.'/test', null, 'test', array()],
+            ['/'.Router::DEFAULT_MODULE.'/test', Router::DEFAULT_MODULE, 'test', array()],
+            ['/'.Router::DEFAULT_MODULE.'/test/foo/bar', Router::DEFAULT_MODULE, 'test', ['foo'=>'bar']],
+        );
+    }
+    
+    /**
+     * @return array
+     */
+    public function providerForCustomRoutes()
+    {
+        return array(
+            ['/another-route.html', 'test', 'route-static'],
+            ['/another-route.html?foo%5B0%5D=bar&foo%5B1%5D=baz', 'test', 'route-static', ['foo'=> ['bar', 'baz']]],
+            ['/test/param/42/', 'test', 'route-with-param', ['a' => 42]],
+            ['/foo-bar-baz/', 'test', 'route-with-params', ['a' => 'foo', 'b' => 'bar', 'c' => 'baz']],
+            ['/test/route-with-other-params/about', 'test', 'route-with-other-params', ['alias' => 'about']],
         );
     }
 }

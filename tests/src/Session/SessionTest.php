f7007b403175cd08f68fb78e60bed1eb02dfddcc
AntonShevchuk <AntonShevchuk@gmail.com>
2014-08-20 13:19:18 +0300

    Added tests for Session package Added method `Registry::__unset()`

---
+++ tests/src/Session/SessionTest.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * @copyright Bluz PHP Team
+ * @link https://github.com/bluzphp/framework
+ */
+
+/**
+ * @namespace
+ */
+namespace Bluz\Tests\Session;
+
+use Bluz\Session\Session;
+use Bluz\Tests\TestCase;
+
+/**
+ * SessionTest
+ *
+ * @package  Bluz\Tests\Session
+ *
+ * @author   Anton Shevchuk
+ * @created  20.08.2014 13:00
+ */
+class SessionTest extends TestCase
+{
+    /**
+     * @var Session
+     */
+    protected $session;
+
+    /**
+     * setUp
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->session = new Session();
+        $this->session->setStore('array');
+    }
+
+    /**
+     * Complex test for setter/getter
+     *
+     * @covers \Bluz\Session\Session::__set()
+     * @covers \Bluz\Session\Session::__get()
+     * @return void
+     */
+    public function testSetGet()
+    {
+        $this->assertNull($this->session->foo);
+
+        $this->session->foo = 'baz';
+
+        $this->assertEquals('baz', $this->session->foo);
+    }
+
+    /**
+     * Complex test for __isset
+     *
+     * @covers \Bluz\Session\Session::__isset()
+     * @return void
+     */
+    public function testIsset()
+    {
+        $this->session->moo = 'maz';
+
+        $this->assertTrue(isset($this->session->moo));
+        $this->assertFalse(isset($this->session->boo));
+    }
+
+    /**
+     * Complex test for __unset
+     *
+     * @covers \Bluz\Session\Session::__unset()
+     * @return void
+     */
+    public function testUnset()
+    {
+        $this->session->moo = 'maz';
+        unset($this->session->moo);
+
+        $this->assertNull($this->session->moo);
+    }
+}

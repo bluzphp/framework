c0b5e39bfa8fcf76ee57e66c7ca0c7a0f63abff8
Anton Shevchuk <AntonShevchuk@gmail.com>
2017-07-18 16:01:19 +0300

    Clean `_bootstrap.php`

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -3,9 +3,3 @@
 // Emulate session
 $_SESSION = [];
 $_COOKIE[session_name()] = uniqid();
-
-// init application
-$env = getenv('BLUZ_ENV') ?: 'testing';
-
-$app = \Bluz\Tests\BootstrapTest::getInstance();
-$app->init($env);
